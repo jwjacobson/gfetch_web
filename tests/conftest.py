d863ac27d4d24bea8e54c17d9fac739e95e5fb08
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-08-24 11:29:46 -0400

    Move temp_files and temp_dirs fixtures to conftest.py, add session scope to temp_files

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,36 @@ def test_client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+@pytest.fixture(scope="session")
+def temp_dirs(tmp_path_factory):
+    """
+    Create temporary directories to delete files from.
+    """
+    attachments_dir = tmp_path_factory.mktemp("attachments")
+    cleaned_email_dir = tmp_path_factory.mktemp("cleaned_emails")
+    raw_email_dir = tmp_path_factory.mktemp("raw_emails")
+
+
+    return {
+        "attachments_dir": attachments_dir,
+        "cleaned_email_dir": cleaned_email_dir,
+        "raw_email_dir": raw_email_dir
+    }
+
+@pytest.fixture
+def temp_files(temp_dirs):
+    """
+    Create fake email and attachment files in the temp directories.
+    """
+    (temp_dirs["attachments_dir"] / "file1.pdf").touch()
+    (temp_dirs["attachments_dir"] / "file2.docx").touch()
+
+    (temp_dirs["cleaned_email_dir"] / "email1.txt").touch()
+    (temp_dirs["cleaned_email_dir"] / "email2.txt").touch()
+
+    (temp_dirs["raw_email_dir"] / "email1.eml").touch()
+    (temp_dirs["raw_email_dir"] / "email2.eml").touch()
+
+    return temp_dirs
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,38 +5,6 @@ import pytest
 from app import app
 from unittest.mock import patch
 
-@pytest.fixture
-def temp_dirs(tmp_path_factory):
-    """
-    Create temporary directories to delete files from.
-    """
-    attachments_dir = tmp_path_factory.mktemp("attachments")
-    cleaned_email_dir = tmp_path_factory.mktemp("cleaned_emails")
-    raw_email_dir = tmp_path_factory.mktemp("raw_emails")
-
-
-    return {
-        "attachments_dir": attachments_dir,
-        "cleaned_email_dir": cleaned_email_dir,
-        "raw_email_dir": raw_email_dir
-    }
-
-@pytest.fixture
-def temp_files(temp_dirs):
-    """
-    Create fake email and attachment files in the temp directories.
-    """
-    (temp_dirs["attachments_dir"] / "file1.pdf").touch()
-    (temp_dirs["attachments_dir"] / "file2.docx").touch()
-
-    (temp_dirs["cleaned_email_dir"] / "email1.txt").touch()
-    (temp_dirs["cleaned_email_dir"] / "email2.txt").touch()
-
-    (temp_dirs["raw_email_dir"] / "email1.eml").touch()
-    (temp_dirs["raw_email_dir"] / "email2.eml").touch()
-
-    return temp_dirs
-
 
 def test_delete_files_empty_dirs(test_client, monkeypatch, temp_dirs):
     monkeypatch.setattr("app.ATTACHMENTS_DIR", temp_dirs["attachments_dir"])
