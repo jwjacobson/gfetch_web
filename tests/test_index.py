aa2072d7c0f1a6ae58e40a923e9e32e52c83c387
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-08-25 11:09:42 -0400

    Add post_no_creds test

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,7 @@
-import ipdb
+import pytest
+
+from app import app
+from unittest.mock import Mock, patch
 
 def test_get(test_client):
     response = test_client.get("/")
@@ -9,3 +12,18 @@ def test_post_empty_request(test_client):
     response = test_client.post("/")
 
     assert response.status_code == 400
+
+def test_post_no_creds(test_client, monkeypatch):
+    mock_creds = None
+    monkeypatch.setattr("app.get_credentials", lambda: mock_creds)
+
+    response = test_client.post("/", data={"email_address": "biff@email.com"})
+
+    assert response.status_code == 302
+
+    with test_client.session_transaction() as session:
+        messages = [
+            message[1] for message in session["_flashes"] if message[0] == "message"
+        ]
+        assert "Failed to obtain credentials." in messages
+
