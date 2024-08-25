06bcb20432214c5ca583205004bd86327625dc0c
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-08-25 10:42:11 -0400

    Create post_empty_request test

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,11 @@
+import ipdb
+
 def test_get(test_client):
     response = test_client.get("/")
     
     assert response.status_code == 200
+
+def test_post_empty_request(test_client):
+    response = test_client.post("/")
+
+    assert response.status_code == 400
