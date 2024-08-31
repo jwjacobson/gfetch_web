b4f1e986281e8845685edaeea2c37799d4853a28
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-08-31 09:35:00 -0400

    Document create_dirs_not_exist test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,20 +10,24 @@ def test_create_dirs_already_exist(monkeypatch, temp_dirs):
 
 
 def test_create_dirs_not_exist(monkeypatch, tmp_path):
+    # Create temporary null paths
     attachments_dir = tmp_path / "attachments"
     clean_email_dir = tmp_path / "cleaned_emails"
     raw_email_dir = tmp_path / "raw_emails"
 
+    # Make sure the directories don't exist
     assert not attachments_dir.exists()
     assert not clean_email_dir.exists()
     assert not raw_email_dir.exists()
 
+    # Point the app at the null directories
     monkeypatch.setattr(app.dir_config, "ATTACHMENTS_DIR", attachments_dir)
     monkeypatch.setattr(app.dir_config, "CLEAN_EMAIL_DIR", clean_email_dir)
     monkeypatch.setattr(app.dir_config, "RAW_EMAIL_DIR", raw_email_dir)
 
     create_dirs(app.dir_config)
 
+    # Make sure the directories exist
     assert attachments_dir.exists()
     assert clean_email_dir.exists()
     assert raw_email_dir.exists()
